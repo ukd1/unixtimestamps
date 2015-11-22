519cea91d8ca836bfa44da8e8a4bd4dfdf871649
Russell Smith <russ@rainforestqa.com>
2015-11-21 18:11:51 -0800

    Switch to streaming for the sitemap stuff

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"io"
 	"os"
 	"strconv"
 	"time"
@@ -11,7 +12,7 @@ import (
 func main() {
 	const RFC2822 = "Mon Jan 02 15:04:05 -0700 2006"
 	const BASE_URL = "https://unixtimestamps.rsmith.co"
-	const SITEMAP_JUMP = 100000
+	const SITEMAP_JUMP = 50000
 
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/*")
@@ -21,35 +22,42 @@ func main() {
 	})
 
 	r.GET("/robots.txt", func(c *gin.Context) {
-		c.String(200, "allow: *\nSitemap: %s/sitemap.txt", BASE_URL)
+		c.String(200, "allow: *\nSitemap: %s/sitemap.xml", BASE_URL)
 	})
 
-	r.GET("/sitemap.txt", func(c *gin.Context) {
-		out := ""
-		i := 0
-		for i < 2147472000 {
-			i += SITEMAP_JUMP
-			out += fmt.Sprintf("%s/sitemap/%d/%d/map.txt\n", BASE_URL, i, i+SITEMAP_JUMP)
-		}
+	r.GET("/sitemap.xml", func(c *gin.Context) {
+		c.Stream(func(w io.Writer) bool {
+			year, month, day := time.Now().Date()
+			t := fmt.Sprintf("%04d-%02d-%02d", year, month, day)
 
-		c.String(200, out)
+			w.Write([]byte("<?xml version=\"1.0\" encoding=\"UTF-8\"?>"))
+			w.Write([]byte("<sitemapindex xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">"))
+
+			// start from somewhere in the middle, as we can only have 500 files
+			i := 0
+			for i < 2147472000 {
+				i += SITEMAP_JUMP
+				w.Write([]byte(fmt.Sprintf("\n<sitemap>\n <loc>%s/sitemaps/%d/%d/map.txt</loc>\n <lastmod>%s</lastmod>\n</sitemap>", BASE_URL, i, i+SITEMAP_JUMP, t)))
+			}
+
+			w.Write([]byte("</sitemapindex>"))
+			return false
+		})
 	})
 
-	r.GET("/sitemap/:from/:to/map.txt", func(c *gin.Context) {
+	r.GET("/sitemaps/:from/:to/map.txt", func(c *gin.Context) {
 		from, _ := strconv.ParseInt(c.Param("from"), 10, 64)
 		to, _ := strconv.ParseInt(c.Param("to"), 10, 64)
 
-		out := ""
+		c.Stream(func(w io.Writer) bool {
+			i := from
+			for i < to {
+				i += 1
+				w.Write([]byte(fmt.Sprintf("%s/unixtimestamp/%d\n", BASE_URL, i)))
+			}
 
-		//c.Stream(step)
-
-		i := from
-		for i < to {
-			i += 1
-			out += fmt.Sprintf("%s/unixtimestamp/%d\n", BASE_URL, i)
-		}
-
-		c.String(200, out)
+			return false
+		})
 	})
 
 	r.GET("/unixtimestamp/:uts", func(c *gin.Context) {
